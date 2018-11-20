c3916d81479740e3c2a63292cb3e488cd5aea085
Michal Barski <michal16292@gmail.com>
2018-11-20 14:27:02 +0200

    mySql changed to mongoDB

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 const express = require('express');
-const mysql = require('mysql');
-
-//Create connection
-const db = mysql.createConnection({
-  host: 'sql7.freemysqlhosting.net',
-  user: 'sql7265004',
-  password: 'L5XmtVVk2f',
-  database: 'sql7265004'
-});
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://atmd_project:atmd1234@cluster0-shard-00-00-mh9ef.mongodb.net:27017,cluster0-shard-00-01-mh9ef.mongodb.net:27017,cluster0-shard-00-02-mh9ef.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true')
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB...', err));
 // Connect
 
-db.connect(err => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('MySql Connected...');
-});
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
