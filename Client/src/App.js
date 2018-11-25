145b90cbab69fbdb48b63eb7104b2f303e1d9613
AlexKre <36233456+AlexKre@users.noreply.github.com>
2018-11-25 14:37:57 +0200

    adding auth

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import Login from "./components/Login";
-
 import {
   BrowserRouter as Router,
   Route,
@@ -8,17 +6,97 @@ import {
   Redirect,
   withRouter
 } from "react-router-dom";
+import Login from "./components/Login";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    id: "",
+    password: "",
+    role: "",
+    token: "",
+    role: "",
+    firstName: "",
+    lastName: "",
+    auth: false,
+    error: false
+  };
+  onChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    let id = this.state.id;
+    let password = this.state.password;
+    fetch("/users/login", {
+      method: "POST",
+      body: JSON.stringify({ id: id, password: password }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      if (response.ok) {
+        response.json().then(json => {
+          this.setState({
+            token: json.token,
+            role: json.role,
+            id: json.id,
+            firstName: json.firstName,
+            lastName: json.lastName,
+            password: "",
+            auth: true,
+            error: false
+          });
+        });
+      } else {
+        this.setState({ error: true });
+      }
+    });
+  };
+  logout = () => {
+    this.setState({ auth: false });
+  };
   render() {
     return (
       <div className="App">
-        <Router>
-          <div>
-            <Route exact path="/" component={Login} />
-          </div>
-        </Router>
+        {this.state.auth == false ? (
+          <form onSubmit={this.onSubmit}>
+            <div>
+              <label htmlFor="id">Username</label>
+              <input
+                name="id"
+                value={this.state.id}
+                onChange={this.onChange}
+                type="text"
+                required="true"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                value={this.state.password}
+                onChange={this.onChange}
+                type="password"
+                required="true"
+              />
+            </div>
+            <button>Submit</button>
+          </form>
+        ) : null}
+
+        <div>
+          {this.state.auth ? (
+            <div>
+              <h4>Welcome {this.state.firstName} </h4>
+              <button onClick={this.logout}>Log out</button>
+            </div>
+          ) : (
+            <h4>You are not logged in</h4>
+          )}
+          {this.state.error ? <h4>Invalid possword or User Id</h4> : null}
+        </div>
       </div>
     );
   }
