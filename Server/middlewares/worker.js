09204a3fd2d3a42d1ded55d2b28cd60753a459c7
Dima Zagorodny <dima@ivtech.co.il>
2018-11-25 10:41:14 +0200

    Hackaton first commit

--- Server/middlewares/worker.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(req, res, next) {
-    if (req.user.role != 'worker') return res.status(403).send('Access denied.');
-
-    next();
-}
--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const config = require('./Server/config/config');
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-// Config server
-config.setRoutes(app);
-config.connectToDB();
 
 // Start server
 app.listen(port, () => console.log(`Server started on port ${port}`));
