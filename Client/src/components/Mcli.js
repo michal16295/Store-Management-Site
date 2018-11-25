f38b546f49a95e726de24cb928fb8ada29b2d7fe
leontomer <41841316+leontomer@users.noreply.github.com>
2018-11-25 13:14:03 +0200

    check

---
+++ Client/src/components/Mcli.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Mcli extends Component {
+  render() {
+    return (
+      <div>
+        <button>Show Customer Card</button>
+        <br />
+        <button>Show Products</button>
+        <br />
+        <button>Contact us</button>
+        <br />
+
+        <button>Show worker list</button>
+        <br />
+        <button>Show our location</button>
+        <br />
+      </div>
+    );
+  }
+}
+
+//ReactDOM.render(<mainMenuClient />, document.getElementById("root"));
+export default Mcli;
+// Learn more about service workers: http://bit.ly/CRA-PWA
