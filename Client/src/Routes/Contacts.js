1140db22a46be2cdbf912c39bf47cb0380105df1
leontomer <41841316+leontomer@users.noreply.github.com>
2018-12-16 20:49:24 +0200

    contact info added

--- Client/src/Routes/Contacts.js
+++ Client/src/Routes/Contacts.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React from "react";
 
-export default function Contacts(){
-    return(
-        <div>Contacts</div>
-    )
+export default function Contacts() {
+  return (
+    <div className="container-login100">
+      <h2>Contact info:</h2>
+
+      <br />
+      <br />
+      <div>
+        <span>
+          <h3>
+            <br />
+            <br />
+            <br />
+            <br /> E-mail: ATMDstore@gmail.com{" "}
+          </h3>
+        </span>
+      </div>
+
+      <div>
+        <span>
+          <h3>
+            {" "}
+            <br />
+            <br />
+            <br />
+            <br /> <br />
+            <br />
+            <br />
+            <br /> Phone number: 08-6448484
+          </h3>
+        </span>
+      </div>
+    </div>
+  );
 }
