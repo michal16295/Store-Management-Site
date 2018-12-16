fac21cbc37437632dedfa1fb6cc9136a4b7ddb8a
AlexKreinis <36419382+AlexKreinis@users.noreply.github.com>
2018-12-16 19:25:47 +0200

    updated on rate(not finished)

--- Client/src/Routes/Workers/Rate.js
+++ Client/src/Routes/Workers/Rate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../../css/Rate.scss';
 
 export default class Rate extends Component {
   state = {
@@ -9,7 +10,8 @@ export default class Rate extends Component {
     this.props.history.push('/RateWorkers');
   };
   onSelect = ({ target }) => {
-    this.setState({ rating: target.val });
+    console.log(target);
+    this.setState({ rating: target.value });
   };
 
   render() {
@@ -17,17 +19,26 @@ export default class Rate extends Component {
     const value = this.state.ratings;
     return (
       <div className="container-login100">
-        <h1>{`Please Rate the User ID :${this.props.match.params.id}`}</h1>
-        <div>
-          <select onChange={this.onSelect}>
-            <option selected={value === ''}>Please select a Rate</option>
-            {rates.map(val => (
-              <option key={val} value={val} selected={val === value}>
-                {val}
-              </option>
-            ))}
-          </select>
-          <button onClick={this.onClick}>Submit</button>
+        <div className="container" className="wrap-login100">
+          <div className="Rate-title">
+            <h1>{`Please Rate the User ID :${this.props.match.params.id}`}</h1>
+          </div>
+
+          <div className="Rate-Selector">
+            <select onChange={this.onSelect}>
+              <option selected={value === ''}>Please select a Rate</option>
+              {rates.map(val => (
+                <option key={val} value={val} selected={val === value}>
+                  {val}
+                </option>
+              ))}
+            </select>
+            <div className="Rate-Button">
+              <button className="buttonR" onClick={this.onClick}>
+                Submit
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     );
