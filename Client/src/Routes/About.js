81ee7a4e38a2f731f5ca402630da390119db2e83
leontomer <41841316+leontomer@users.noreply.github.com>
2018-12-16 20:09:20 +0200

    about page added

--- Client/src/Routes/About.js
+++ Client/src/Routes/About.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
-export default function About(){
-    return(
-        <di>About</di>
-    )
+export default function About() {
+  return (
+    <div className="container-login100">
+      <br />
+      <h3>
+        ATMD is a store managment website. it was developed in 2018, by Alex
+        Kreinis, Tomer Leon, Michal Barski and Dima Zagorodny, in order to make
+        the life of managers, workers, and costumers easier in their store, by
+        giving them tools to manage items in the store, see clients lists,
+        manage points, and much more!
+      </h3>
+      <br />
+      <br />
+    </div>
+  );
 }
