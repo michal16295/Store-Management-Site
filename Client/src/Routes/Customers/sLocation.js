ab9badc907e349fe86a57baaeb5f663ab9126ec7
AlexKreinis <36419382+AlexKreinis@users.noreply.github.com>
2019-01-05 23:01:41 +0200

    location css

--- Client/src/Routes/Customers/sLocation.js
+++ Client/src/Routes/Customers/sLocation.js
@@ -6,7 +6,7 @@ class sLocation extends Component {
     return (
       <div className="container-login100">
         <div class="h4C">
-          <h4>Adress: David ha-Re'uveni St 25, Be'er Sheva, Israel</h4>
+          <h4>Address: David ha-Re'uveni St 25, Be'er Sheva, Israel</h4>
         </div>
         <div>
           <img src="https://cascade.madmimi.com/promotion_images/2120/8750/original/Screenshot_beersheva_25_beis_midrash.jpg?1503575695" />
