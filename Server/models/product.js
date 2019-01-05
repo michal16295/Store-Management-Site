dac345c7a429c5e0d81476dc6bc45892e9568f72
leontomer <41841316+leontomer@users.noreply.github.com>
2019-01-05 22:30:45 +0200

    string min length corrected in products.js

--- Server/models/product.js
+++ Server/models/product.js
@@ -1,48 +1,52 @@
-const mongoose = require('mongoose');
-const Joi = require('joi');
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const Schema = mongoose.Schema;
 
 const Product_Schema = new Schema({
-    id: {
-        type: Number,
-        min: 1,
-        required: true
-    },
-    name: {
-        type: String,
-        minlength: 2, 
-        required: true
-    },
-    buyingPrice: {
-        type: Number,
-        min: 0,
-        required: true
-    },
-    sellingPrice:{
-        type: Number,
-        min: 0,
-        required: true
-    },
-    quantity: {
-        type: Number,
-        min: 0,
-        required: true
-    }
+  id: {
+    type: Number,
+    min: 1,
+    required: true
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    required: true
+  },
+  buyingPrice: {
+    type: Number,
+    min: 0,
+    required: true
+  },
+  sellingPrice: {
+    type: Number,
+    min: 0,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    min: 0,
+    required: true
+  }
 });
 
 function ValidateNewProduct(req) {
-    const schema = {
-      id: Joi.number().required(),
-      name: Joi.string()
-        .min(2)
-        .max(255)
-        .required(),
-      buyingPrice: Joi.number().min(0).required(),
-      sellingPrice: Joi.number().min(0).required()
-    };
-    return Joi.validate(req, schema);
-  }
+  const schema = {
+    id: Joi.number().required(),
+    name: Joi.string()
+      .min(1)
+      .max(255)
+      .required(),
+    buyingPrice: Joi.number()
+      .min(0)
+      .required(),
+    sellingPrice: Joi.number()
+      .min(0)
+      .required()
+  };
+  return Joi.validate(req, schema);
+}
 
-exports.Product = mongoose.model('Product' , Product_Schema);
+exports.Product = mongoose.model("Product", Product_Schema);
 exports.ValidateNewProduct = ValidateNewProduct;
