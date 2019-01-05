f2290054f91cf8fce7dbc5add11556c74d10f331
AlexKreinis <36419382+AlexKreinis@users.noreply.github.com>
2019-01-05 22:56:46 +0200

    location css

--- Client/src/Routes/Customers/sLocation.js
+++ Client/src/Routes/Customers/sLocation.js
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import '../../css/location.scss';
 class sLocation extends Component {
   render() {
     return (
       <div className="container-login100">
-        <h3>Adress </h3>
-        <div>
-          <h4>: David ha-Re'uveni St 25, Be'er Sheva, Israel</h4>
+        <div class="h4C">
+          <h4>Adress: David ha-Re'uveni St 25, Be'er Sheva, Israel</h4>
         </div>
-
-        <img src="https://cascade.madmimi.com/promotion_images/2120/8750/original/Screenshot_beersheva_25_beis_midrash.jpg?1503575695" />
+        <div>
+          <img src="https://cascade.madmimi.com/promotion_images/2120/8750/original/Screenshot_beersheva_25_beis_midrash.jpg?1503575695" />
+        </div>
       </div>
     );
   }
